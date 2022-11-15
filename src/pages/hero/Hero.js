ca8df1a15591881f97b096a3b072544c33b78d09
Lucas Alves <lucasalves94@gmail.com>
2022-11-15 18:36:29 -0300

    fix overlay bug & small gap under hero cards

--- src/pages/hero/Hero.js
+++ src/pages/hero/Hero.js
@@ -24,17 +24,20 @@ export default function Hero() {
     
     return (
         
-        <div className="hero-info">
-        <img className="hero-img" src={"https://api.opendota.com" + selectedHero.img}/>
-        <h1> {selectedHero.localized_name} </h1>
-        <p className="title-hero-page">Primary attribute:</p>
-        <p> {selectedHero.primary_attr.charAt(0).toUpperCase()+selectedHero.primary_attr.slice(1)}</p> 
-        <p className="title-hero-page"> Attack type: </p>
-        <p> {selectedHero.attack_type}</p>
-        <ul className="hero-roles-ul">
-            <p className="title-hero-page">Roles:</p>
-            {selectedHero.roles.map(item => <li className="hero-roles">{item}</li>)}
-        </ul>
+        <div >
+            <div className="hero-info">
+                <img className="hero-img" src={"https://api.opendota.com" + selectedHero.img}/>
+                <h1> {selectedHero.localized_name} </h1>
+                <p className="title-hero-page">Primary attribute:</p>
+                <p> {selectedHero.primary_attr.charAt(0).toUpperCase()+selectedHero.primary_attr.slice(1)}</p> 
+                <p className="title-hero-page"> Attack type: </p>
+                <p> {selectedHero.attack_type}</p>
+                <ul className="hero-roles-ul">
+                    <p className="title-hero-page">Roles:</p>
+                    {selectedHero.roles.map(item => <li className="hero-roles">{item}</li>)}
+                </ul>
+            </div>
+            <span className="empty-space"></span>
         </div>
         
     ) 
