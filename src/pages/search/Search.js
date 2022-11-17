e34880dfb48a0d44663c25a0d33c95f0c680e1da
Lucas Alves <lucasalves94@gmail.com>
2022-11-16 22:16:02 -0300

    crazy idea

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -15,7 +15,34 @@ export default function Search() {
     const query = queryParams.get('q')
 
     const url = 'https://dota-guides.netlify.app/search?q=' + query
-    const { error, isPending, data } = useFetch(url, method = 'GET')
+    
+    const [data, setData] = useState(null)
+    const [isPending, setIsPending] = useState(false)
+    const [error, setError] = useState(false)
+
+    useEffect(() => {
+        setIsPending(true)
+
+        const unsub = projectFirestore.collection('guides').onSnapshot((snapshot) => {
+            if (snapshot.empty) {
+                setError('No guides to load')
+                setIsPending(false)
+            } else {
+                let results = [] 
+                snapshot.docs.forEach(doc => {
+                    results.push({ id: doc.id, ...doc.data() })
+                })
+                setData(results)
+                setIsPending(false)
+            }
+        }, (err) => {
+            setError(err.message)
+            setIsPending(false)
+        })
+
+        return () => unsub()
+
+    }, [])
 
 
     return (
