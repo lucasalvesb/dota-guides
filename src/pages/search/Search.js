d2548c1d6d95ad182b49f4881d908c8925ba6055
Lucas Alves <lucasalves94@gmail.com>
2022-11-17 11:53:55 -0300

    ''error''

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -11,14 +11,14 @@ import './Search.css'
 import GuideData from '../guidedata/GuideData'
 
 export default function Search() {
-/*     const queryString = useLocation().search
+    const queryString = useLocation().search
     const queryParams = new URLSearchParams(queryString)
     const query = queryParams.get('q')
 
-    const url = 'https://dota-guides.netlify.app/search?q=' + query
-    const { error, isPending, data } = useFetch(url) */
+    const url = 'https://firestore.googleapis.com/google.firestore.v1.Firestore/Listen/channel?database=projects%2Fcooking-ninja-site-42f4a%2Fdatabases%2F(default)&VER=8&RID=21958&CVER=22&X-HTTP-Session-Id=gsessionid&%24httpHeaders=X-Goog-Api-Client%3Agl-js%2F%20fire%2F8.5.0%0D%0AContent-Type%3Atext%2Fplain%0D%0AX-Firebase-GMPID%3A1%3A1081517467572%3Aweb%3Ab54c3037a97d1ef20434e1%0D%0A&zx=qihmsbz3hgf&t=1' + query
+    const { error, isPending, data } = useFetch(url)
 
-    const [data, setData] = useState(null)
+/*     const [data, setData] = useState(null)
     const [isPending, setIsPending] = useState(false)
     const [error, setError] = useState(false)
 
@@ -27,7 +27,7 @@ export default function Search() {
 
         const unsub = projectFirestore.collection('guides').onSnapshot((snapshot) => {
             if (snapshot.empty) {
-                setError('No guides to load')
+                setError('No guides found')
                 setIsPending(false)
             } else {
                 let results = [] 
@@ -59,7 +59,7 @@ export default function Search() {
                 const guides = snapshot.docs.map(doc => doc.data())
                 console.log(guides)
             })
-    }
+    } */
 
     return (
         <div>
@@ -67,7 +67,6 @@ export default function Search() {
             {error && <p className="error">{error}</p>}
             {isPending && <p className="loading">Loading...</p>}
             {data && <GuideList guides={data} />}
-            <div> {guides} </div>
         </div>
     )
 }
